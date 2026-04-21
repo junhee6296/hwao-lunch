3fdd86c4fcba450697b0b71f7c25bad6ceecba5e
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2026-04-21 11:06:26 +0900

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const nodemailer = require('nodemailer');
 const fs = require('fs');
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
